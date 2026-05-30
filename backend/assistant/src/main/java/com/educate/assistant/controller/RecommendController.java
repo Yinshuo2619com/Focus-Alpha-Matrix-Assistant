5c3de87712d3808009b08b2015d9eeb0677fb60e
YinShuo <19392735072@163.com>
2026-05-30 19:27:02 +0800

    添加电费功能，优化了移动端体验

--- backend/assistant/src/main/java/com/educate/assistant/controller/RecommendController.java
+++ backend/assistant/src/main/java/com/educate/assistant/controller/RecommendController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+// import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
